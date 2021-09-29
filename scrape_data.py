b70e7a496b99427c54c39f9d45134b90865ec5c6
Karl Broman <kbroman@gmail.com>
2021-09-29 15:43:54 -0500

    scrape_data.py: grab dates; combine into a single data frame

--- scrape_data.py
+++ scrape_data.py
@@ -15,22 +15,28 @@ def extract_regex(pattern, value):
     m = re.search(pattern, value)
     if m:
         g = m.groups()
-        return [int(g[0]), float(g[1])]
+        return [int(g[0])]
     else:
-        return [0, 0.0]
+        return [0]
 
 def extract_numbers(s):
     # In the format: 'Students: 0 positive tests<br>7-day average: 4.9<br><br>Employees: 0 positive tests<br>7-day average: 1.0'
     # But may be missing employees
-    students = extract_regex(r"Students: (\d+) [^<]+<br>7-day average: (\d+\.\d+)", s)
-    employees = extract_regex(r"Employees: (\d+) [^<]+<br>7-day average: (\d+\.\d+)", s)
+    students = extract_regex(r"Students: (\d+) ", s)
+    employees = extract_regex(r"Employees: (\d+) ", s)
     return students + employees
 
 def extract_data(chart):
+    # grab date (like "August 7")
+    dates = chart.xpath('//g/@data-tooltip_label')
+    dates_data = pd.DataFrame(dates)
+    dates_data.columns = ['date']
+
     tooltips = chart.xpath('//g/@data-tooltip_annotation')
     data = pd.DataFrame([extract_numbers(x) for x in tooltips])
-    data.columns = ['students', 'students_7day_average', 'employees', 'employees_7day_average']
-    return data
+    data.columns = ['students', 'employees']
+
+    return pd.concat([dates_data, data], axis=1)
 
 positive_chart = r.html.find('.svg-bar-chart', first=True)
 tests_chart = r.html.find('#chart-covid-tests', first=True)
@@ -38,5 +44,8 @@ tests_chart = r.html.find('#chart-covid-tests', first=True)
 positive = extract_data(positive_chart)
 tests = extract_data(tests_chart)
 
-print(positive)
-print(tests)
+positive.columns = ['date', 'pos_students', 'pos_employees']
+tests.columns = ['date', 'tot_students', 'tot_employees']
+
+data = pd.concat([positive, tests], axis=1)
+print(data)
